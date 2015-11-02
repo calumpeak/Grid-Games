a2dc5c1fe567022d7067cb5fa226c16760f4462a
Calum Peak <calumpeak@gmail.com>
2015-11-02 14:02:35 +0000

    Write and show messages

--- src/logic/js/message.js
+++ src/logic/js/message.js
@@ -21,6 +21,29 @@ logic.message = function message (window, document, utils, dom, events) {
         this.elem = options.elem;
     }
 
+    /**
+     * Writes a message to the given elements
+     *
+     * @for Messages
+     * @method writeMessage
+     */
+    Messages.prototype.writeMessage = function writeMessage (message) {
+        this.elem.innerHTML = message;
+    };
+
+    /**
+     * Shows the message based on the key passed
+     * e.g. "level.up" --> "Level Up!!"
+     *
+     * @for Messages
+     * @method showMessage
+     */
+    Messages.prototype.showMessage = function showMessage (key) {
+        key = key.split(".");
+
+        this.writeMessage(this.messages[key[0]][key[1]]);
+    };
+
     function createMessages (options) {
         return new Messages(options);
     }
